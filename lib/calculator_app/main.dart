dd4d5b7567471a1ddff45f72e2662e36f0ff7d35
Alex R <alexregianideveloper@outlook.com>
2023-02-07 16:55:30 -0400

    split calculator functions gave each functions it's own .dart file

--- lib/calculator_app/main.dart
+++ lib/calculator_app/main.dart
@@ -14,14 +14,10 @@ void main() {
     stdout.write(
         '\nWelcome to calculator, choose 2 numbers to perform the calculation on:\n');
 
-    stdout.write('choose first number:\n');
-    number1 = double.parse(stdin.readLineSync()!);
 
-    stdout.write('choose second number:\n');
-    number2 = double.parse(stdin.readLineSync()!);
 
     stdout.write(
-        'Choose your operation: (a)dd, (s)ubtract, (d)ivide or (m)ultiply\n');
+        '\nChoose your operation: (a)dd, (s)ubtract, (d)ivide or (m)ultiply\n');
 
     var operationChoice = stdin.readLineSync();
 
@@ -33,6 +29,8 @@ void main() {
       result = divide(number1, number2);
     } else if (operationChoice == 'm') {
       result = multiply(number1, number2);
+    } else {
+      stdout.write('wrong input, try again');
     }
 
     stdout.write('$result');
